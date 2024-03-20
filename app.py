3d7b7bcd883013e7ce1f11b9d67d81077c09ab06
Praggya <119279862+praggyaverma@users.noreply.github.com>
2024-03-20 15:47:34 +0000

    new commit

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ try:
 except:
     pass
 
+st.title("Research Paper Chatbot")
+st.write("Chat with any research paper uploaded in PDF format.")
+api_key = st.text_input("Enter your OpenAI API key", type = "password")
+client = OpenAI(api_key = api_key)
+
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 if uploaded_file is not None:
     with open("pdfs/uploaded_file.pdf", "wb") as f:
@@ -26,3 +31,68 @@ if uploaded_file is not None:
 else:
     st.warning("Please upload a PDF file.")
 
+pdf = uploaded_file
+
+
+if (api_key == ""):
+    pass
+elif st.button("Research this paper!"):
+    pdf = os.listdir("pdfs")[0]
+    pdf = f"pdfs/{pdf}"
+    with st.spinner("Embedding document... This may take a while"):
+        loader = UnstructuredPDFLoader(f"{pdf}")
+        data = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(data)
+        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
+        vectorstore = FAISS.from_texts(texts=[t.page_content for t in texts], embedding=embeddings)
+    
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Enter your query"):
+        docs = vectorstore.similarity_search(query, 4)
+        system_content = f"""
+        You are a helpful assistant performing Retrieval-augmented generation (RAG).
+        You will be given a user query and some text. 
+        Analyse the text and answer the user query. 
+        If the answer is not present within the text, say that the given question cannot be answered, 
+        dont make up stuff on your own
+        """
+        user_content = f"""
+            Question: {prompt}
+            Do not return the question in the response, please. 
+            ======
+            Supporting texts:
+            1. {docs[0].page_content}
+            2. {docs[1].page_content}
+            3. {docs[2].page_content}
+            4. {docs[3].page_content}
+            ======
+            """
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                
+                messages = [{"role": "system", "content": f"{system_content}"},
+                {"role": "assistant", "content": f"{user_content}"}],
+                
+                stream=True,
+            ):
+                full_response += (response.choices[0].delta.content or "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
